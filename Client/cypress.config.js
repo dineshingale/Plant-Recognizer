8ea66509a899aa5e636b1c3002c04d402fb490db
Dinesh <dineshingale2003@gmail.com>
2026-01-06 11:33:21 +0530

    fix(ci): disable cypress support file requirement

--- Client/cypress.config.js
+++ Client/cypress.config.js
@@ -4,6 +4,7 @@ export default defineConfig({
     projectId: "fq3a65",
     e2e: {
         baseUrl: 'http://127.0.0.1:4173',
+        supportFile: false,
         setupNodeEvents(on, config) {
             // implement node event listeners here
         },
